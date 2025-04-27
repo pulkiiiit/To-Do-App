409d10dad5b93c71cc912596d05fcf7f5156c687
Pulkit Shukla <pulkitshukla001@gmail.com>
2025-04-28 02:20:19 +0530

    tasks are added

--- script.js
+++ script.js
@@ -5,36 +5,42 @@ const task = document.createElement("div");
 
 // variables
 let taskText = "";
+let taskCounter = 0;
 
 taskButton.addEventListener("click", addTask);
 
 function addTask() {
-  taskText = taskInput.value; 
+  taskText = taskInput.value;
   createTask(taskText);
   taskInput.value = "";
 }
 
 function createTask(task) {
-  const toDo = document.createElement("p")
-  toDo.className = "task"
-  toDo.textContent = task;
-  document.getElementById("tasks").append(toDo);
-  const options = document.createElement("div")
-  options.className = "options"
-  options.id = "opitons1"
-  document.getElementById("tasks").append(options)
-  const deleteButton = document.createElement("button")
-  deleteButton.id = "deleteButton"
-  document.getElementById("opitons1").append(deleteButton);
-  const deleteIcon = document.createElement("i");
-  deleteIcon.className  = "fa-solid fa-trash"
-  document.getElementById("deleteButton").append(deleteIcon);
-  const cheakButton = document.createElement("button")
-  cheakButton.id = "checkButton"
-  document.getElementById("opitons1").append(cheakButton);
-  const CheckIcon = document.createElement("i");
-  CheckIcon.className = "fa-solid fa-check"
-  document.getElementById("checkButton").append(CheckIcon);
-
-
+  if (task != "") {
+    taskCounter++;
+    const item = document.createElement("div");
+    item.id = "item" + taskCounter;
+    item.className = "items";
+    tasks.appendChild(item);
+    const toDo = document.createElement("p");
+    toDo.className = "task";
+    toDo.textContent = task;
+    item.appendChild(toDo);
+    const options = document.createElement("div");
+    options.className = "options";
+    options.id = "opitons" + taskCounter;
+    item.appendChild(options);
+    const deleteButton = document.createElement("button");
+    const deleteButtonId = (deleteButton.id = "deleteButton" + taskCounter);
+    options.appendChild(deleteButton);
+    const deleteIcon = document.createElement("i");
+    deleteIcon.className = "fa-solid fa-trash";
+    deleteButton.appendChild(deleteIcon);
+    const cheakButton = document.createElement("button");
+    cheakButton.id = "checkButton" + taskCounter;
+    options.appendChild(cheakButton);
+    const CheckIcon = document.createElement("i");
+    CheckIcon.className = "fa-solid fa-check";
+    cheakButton.appendChild(CheckIcon);
+  }
 }
