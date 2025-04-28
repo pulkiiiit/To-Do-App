f324e883a16f3f4ec6c0e8c0e8e54b45f78608c2
Pulkit Shukla <pulkitshukla001@gmail.com>
2025-04-28 05:30:47 +0530

    check and delete functionality added

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // constants
 const taskButton = document.getElementById("taskButton");
 const taskInput = document.getElementById("taskText");
+const tasks = document.getElementById("tasks");
 const task = document.createElement("div");
 
 // variables
 let taskText = "";
 let taskCounter = 0;
+let active = true;
 
 taskButton.addEventListener("click", addTask);
 
@@ -25,14 +27,19 @@ function createTask(task) {
     const toDo = document.createElement("p");
     toDo.className = "task";
     toDo.textContent = task;
+    toDo.style.fontSize = "22px";
+    toDo.style.color = "green";
     item.appendChild(toDo);
     const options = document.createElement("div");
     options.className = "options";
     options.id = "opitons" + taskCounter;
     item.appendChild(options);
     const deleteButton = document.createElement("button");
-    const deleteButtonId = (deleteButton.id = "deleteButton" + taskCounter);
+    deleteButton.id = "deleteButton" + taskCounter;
     options.appendChild(deleteButton);
+    deleteButton.onclick = function () {
+      tasks.removeChild(item);
+    };
     const deleteIcon = document.createElement("i");
     deleteIcon.className = "fa-solid fa-trash";
     deleteButton.appendChild(deleteIcon);
@@ -42,5 +49,22 @@ function createTask(task) {
     const CheckIcon = document.createElement("i");
     CheckIcon.className = "fa-solid fa-check";
     cheakButton.appendChild(CheckIcon);
+    cheakButton.onclick = function () {
+      if (active === true) {
+        toDo.style.textDecoration = "line-through";
+        toDo.style.color = "red";
+        active = false;
+      } else {
+        toDo.style.textDecoration = "none";
+        toDo.style.color = "green";
+        active = true;
+      }
+    };
+  }
+
+  function deleteAll () {
+    for (let index = 1; index < taskCounter; index++) {
+      
+    }
   }
 }
